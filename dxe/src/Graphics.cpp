cf2ea5fcfce24a9baf45b17de04c80921d6e1b1c
Le Vu Duc Hung <kwonjiyong.gd2021@gmail.com>
2024-02-25 01:10:58 +0700

    some experiments

--- dxe/src/Graphics.cpp
+++ dxe/src/Graphics.cpp
@@ -113,17 +113,32 @@ namespace dxe
 	{
 		struct Vertex
 		{
-			f32 x;
-			f32 y;
+			struct
+			{
+				f32 x;
+				f32 y;
+			} pos;
+
+			struct
+			{
+				unsigned char r;
+				unsigned char g;
+				unsigned char b;
+				unsigned char a;
+			} color;
 		};
 
 		// create vertex buffer (1 2d triangle at center of screen)
-		const Vertex vertices[] =
+		Vertex vertices[] =
 		{
-			{ 0.0f,0.5f },
-			{ 0.5f,-0.5f },
-			{ -0.5f,-0.5f },
+			{ 0.0f, 0.5f, 255, 0, 0, 0},
+			{ 0.5f, -0.5f, 0, 255, 0, 0 },
+			{ -0.5f, -0.5f, 0, 0, 255, 0 },
+			{ -0.3f, 0.3f, 0, 255, 0, 0 },
+			{ 0.3f, 0.3f, 0, 0, 255, 0 },
+			{ 0.0f, -0.8f, 255, 0, 0, 0},
 		};
+		vertices[0].color.g = 255;
 
 		wrl::ComPtr<ID3D11Buffer> pVertexBuffer;
 		D3D11_BUFFER_DESC bd = {};
@@ -138,6 +153,29 @@ namespace dxe
 		sd.pSysMem = vertices;
 
 		DXE_GFX_THROW_INFO(pDevice->CreateBuffer(&bd, &sd, &pVertexBuffer));
+
+		// create index buffer
+		const unsigned short indices[] =
+		{
+			0,1,2,
+			0,2,3,
+			0,4,1,
+			2,1,5,
+		};
+		wrl::ComPtr<ID3D11Buffer> pIndexBuffer;
+		D3D11_BUFFER_DESC ibd = {};
+		ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+		ibd.Usage = D3D11_USAGE_DEFAULT;
+		ibd.CPUAccessFlags = 0u;
+		ibd.MiscFlags = 0u;
+		ibd.ByteWidth = sizeof(indices);
+		ibd.StructureByteStride = sizeof(unsigned short);
+		D3D11_SUBRESOURCE_DATA isd = {};
+		isd.pSysMem = indices;
+		DXE_GFX_THROW_INFO(pDevice->CreateBuffer(&ibd, &isd, &pIndexBuffer));
+
+		// bind index buffer
+		pContext->IASetIndexBuffer(pIndexBuffer.Get(), DXGI_FORMAT_R16_UINT, 0u);
 
 		// Bind vertex buffer to pipeline
 		const UINT stride = sizeof(Vertex);
@@ -166,6 +204,7 @@ namespace dxe
 		const D3D11_INPUT_ELEMENT_DESC ied[] =
 		{
 			{ "Position",0,DXGI_FORMAT_R32G32_FLOAT,0,0,D3D11_INPUT_PER_VERTEX_DATA,0 },
+			{ "Color",0,DXGI_FORMAT_R8G8B8A8_UNORM,0,8,D3D11_INPUT_PER_VERTEX_DATA,0 },
 		};
 		DXE_GFX_THROW_INFO(pDevice->CreateInputLayout(
 			ied, (UINT)std::size(ied),
@@ -194,6 +233,6 @@ namespace dxe
 		pContext->RSSetViewports(1u, &vp);
 
 
-		DXE_GFX_THROW_INFO_ONLY(pContext->Draw((UINT)std::size(vertices), 0u));
+		DXE_GFX_THROW_INFO_ONLY(pContext->DrawIndexed((UINT)std::size(indices), 0u, 0u));
 	}
 }
